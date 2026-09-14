41c3f3e65a9be2e3ec52b9de686d7d4a3750d9c5
Sourabh Jain <sourjain2002@gmail.com>
2026-09-14 13:13:05 +0530

    fix: refresh cached app assets for data IO

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE='linktracer-v2';
-const APP=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest'];
+const CACHE='linktracer-v3';
+const APP=['/','/index.html','/styles.css','/app.js','/io.js','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   const url=new URL(e.request.url);
